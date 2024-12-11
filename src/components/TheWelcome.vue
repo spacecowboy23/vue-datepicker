8d6246354a7d0a699a973dfe6408bba3b2873714
doc <doc.kuehn@nexgo.de>
2024-12-11 15:28:20 +0100

    updates

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <VueDatePicker 
+    v-model="date" 
+    auto-apply 
+    @update:model-value="onHandleUpdateValue"
+  />
+</template>
+
+<script setup lang="ts">
+  import VueDatePicker from '@vuepic/vue-datepicker';
+  import '@vuepic/vue-datepicker/dist/main.css'
+  import { ref } from 'vue';
+  
+  const date = ref(new Date());
+
+  const onHandleUpdateValue = () => {
+    alert('updated model Value - showcase for iOS Safari Bug');
+  }
+</script>
---
+++ src/components/e2e/TheWelcome.e2e.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('event @update:model-value', async ({ page }) => {
+  // set pre-defined date  
+  await page.clock.install({ time: new Date('2024-02-02T08:00:00') });
+
+  await page.goto('https://showcase-datepicker.vercel.app/');
+
+  await page.locator('[data-test-id="dp-input"]').click();
+
+  page.once('dialog', dialog => {
+    dialog.dismiss().catch(() => {});
+  });
+
+  await page.locator('[data-test-id^="Mon Dec 23 2024"]').getByText('23').click();
+
+  await expect(page.locator('[data-test-id="dp-input"]')).toHaveValue(/12\/23\/2024/)
+});
+
